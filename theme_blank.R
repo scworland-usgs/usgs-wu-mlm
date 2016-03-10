e416da4df375d0ad2d5464d13284a5ee16d8cee5
scworland-usgs <scworland@usgs.gov>
2016-03-10 05:45:11 -0600

    added text.size to map blank theme

--- theme_blank.R
+++ theme_blank.R
@@ -1,9 +1,9 @@
-theme_blank <- function(){
+theme_blank <- function(text.size=18){
   require(grid)
   theme(panel.background = element_rect(fill = "transparent",color=NA),
         plot.background = element_rect(fill = "transparent",color=NA),
         legend.background = element_rect(fill = "transparent",color=NA),
-        text = element_text(size=18),
+        text = element_text(size=text.size),
         axis.ticks = element_blank(),
         axis.text = element_blank(),
         axis.title = element_blank(),
